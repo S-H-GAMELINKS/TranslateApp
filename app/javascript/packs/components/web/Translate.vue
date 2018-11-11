a757aa0cf7b8738e601d863eb3303e2c40625eed
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-11 14:38:38 +0900

    using vue-dictaphone

--- app/javascript/packs/components/web/Translate.vue
+++ app/javascript/packs/components/web/Translate.vue
@@ -1,11 +1,32 @@
 <template>
     <div class="container">
         <h1>Translate</h1>
+        <vue-dictaphone @stop="handleRecording($event)">
+  <template slot-scope="{ isRecording, startRecording, stopRecording, deleteRecording }">
+    <button v-if="!isRecording" @click="startRecording">Start recording</button>
+    <button v-else @click="stopRecording">Stop recording</button>
+  </template>
+</vue-dictaphone>
+
+<vue-dictaphone-spectrum-analyser/>
+
+<template v-if="audioSource">
+  <audio :src="audioSource" controls></audio>
+</template>
     </div>
 </template>
 
 <script>
 export default {
-
+    data: function() {
+        return {
+            audioSource: null
+        }
+    },
+    methods: {
+        handleRecording({ blob, src }) {
+            this.audioSource = src;
+    }
+  }
 }
 </script>
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue/dist/vue.esm';
 import Router from './router/router';
 import Header from './components/layouts/Header.vue';
 
+import VueDictaphone from "vue-dictaphone";
+Vue.use(VueDictaphone);
+
 const app = new Vue({
     el: '#app',
     router: Router,
